d36ba536c191bc1b8812246c8d4b6f4d55321101
J.D. Healy <jdhealy@gmail.com>
2016-11-08 14:12:43 -0500

    Tests: Move directories to conform to Swift Package Manager test module requirements.
    Generate the framework target automatically and manually append any test target.
    
    See: <https://github.com/apple/swift-package-manager/pull/566>.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,13 @@
 import PackageDescription
 
-let package = Package(
+var package = Package(
 	name: "PrettyColors",
-	exclude: ["Supporting Files", "Tests/Supporting Files"],
-	targets: []
+	exclude: ["Supporting Files", "Tests/Supporting Files"]
 )
+
+package.targets += [
+	Target(
+		name: "UnitTests",
+		dependencies: ["PrettyColors"]
+	)
+]
