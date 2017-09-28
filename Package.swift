84d37e5a35cd7f3cb8e535d948f001ce2b2f9b01
J.D. Healy <jdhealy@gmail.com>
2017-09-28 17:39:55 -0400

    Non-Darwin: Swift Package Manager: Tests: Add generated `LinuxMain.swift`.
    Include [Sourcery][1] template: `LinuxMain.stencil`.
    
    [1]: https://github.com/krzysztofzablocki/Sourcery/

--- Package.swift
+++ Package.swift
@@ -4,19 +4,12 @@ import PackageDescription
 
 var package = Package(
 	name: "PrettyColors",
-	exclude: ["Supporting Files", "Tests/Supporting Files"]
+	exclude: ["Supporting Files", "Tests"]
 )
 
-// NOTE: Unchecked presumption that we are not cross-compiling tests to be
-// run on another platform. The conditional compilation check for `macOS`
-// is intended to only provide support for Darwin XCTest for the time being.
-#if os(macOS)
-	package.targets += [
-		Target(
-			name: "UnitTests",
-			dependencies: ["PrettyColors"]
-		)
-	]
-#else
-	package.exclude += ["Tests"]
-#endif
+package.targets += [
+	Target(
+		name: "UnitTests",
+		dependencies: ["PrettyColors"]
+	)
+]
